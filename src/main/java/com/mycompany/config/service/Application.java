4de1a071e6052497ace026b103d4fbd820df2a24
NartovEvgenii <evgeniynartov36rus@gmail.com>
2020-12-10 20:16:47 +0300

    add DockerFile

--- src/main/java/com/mycompany/config/service/Application.java
+++ src/main/java/com/mycompany/config/service/Application.java
@@ -4,14 +4,12 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-            System.out.println("Hello world");
-                        
+	public static void main(String[] args) {            
+            SpringApplication.run(Application.class, args);  
 	}
 
 }
--- src/main/java/com/mycompany/config/service/ClassExample.java
+++ src/main/java/com/mycompany/config/service/ClassExample.java
@@ -1,17 +1,18 @@
 package com.mycompany.config.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class ClassExample {
 
+    @RestController
+    public class HelloController {
 
-@RestController
-public class HelloController {
+        @RequestMapping("/")
+        public String index() {
+            return "Hello World!";
+        }
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
-
-}
+    }
 }
